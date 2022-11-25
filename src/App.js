c79176502dab713be4382f3891b455d82d8337a9
Dilip <dilippoudel578@gmail.com>
2022-11-25 20:48:00 +0200

    Add random selection function

--- src/App.js
+++ src/App.js
@@ -1,86 +1,30 @@
 import { useState } from 'react'
-const Heading = ({ text }) => <h2>{text}</h2>
-const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
-const StatisticLine = ({ text, value }) => {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>{text}</td>
-          <td>{value}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-// Statics components
-const Statistics = (props) => {
-  let { good, neutral, bad } = props.countFeedBack
-  let goodFeedback = good - bad
-  let totalFeedback = good + bad + neutral
-  let doWeHaveFeedback = good || bad || neutral
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const nextAnecdoteHandler = () => {
+    let random = Math.floor(Math.random() * anecdotes.length + 1)
+    setSelected(random - 1)
+    console.log(random)
+  }
   return (
     <div>
-      <Heading text="statistics" />
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={totalFeedback} />
-
-      {doWeHaveFeedback ? (
-        <div>
-          <StatisticLine
-            text="average"
-            value={(goodFeedback / totalFeedback).toFixed(1)}
-          />
-          <StatisticLine
-            text="positive"
-            value={`${((good / totalFeedback) * 100).toFixed(1)}%`}
-          />
-        </div>
-      ) : (
-        <p>No feedback given</p>
-      )}
+      {anecdotes[selected]}
+      <div>
+        <button onClick={nextAnecdoteHandler}>next anecdote</button>
+      </div>
     </div>
   )
 }
-const App = () => {
-  const [countFeedBack, setCountFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  })
 
-  const goodFeedbackHandler = () => {
-    let newState = {
-      ...countFeedBack,
-      good: countFeedBack.good + 1,
-    }
-    setCountFeedback(newState)
-  }
-  const neutralFeedbackHandler = () => {
-    let newState = {
-      ...countFeedBack,
-      neutral: countFeedBack.neutral + 1,
-    }
-    setCountFeedback(newState)
-  }
-  const badFeedbackHandler = () => {
-    let newState = {
-      ...countFeedBack,
-      bad: countFeedBack.bad + 1,
-    }
-    setCountFeedback(newState)
-  }
-
-  return (
-    <div className="container">
-      <Heading text="give feedback" />
-      <Button text="good" onClick={goodFeedbackHandler} />
-      <Button text="neutral" onClick={neutralFeedbackHandler} />
-      <Button text="bad" onClick={badFeedbackHandler} />
-      <Statistics countFeedBack={countFeedBack} />
-    </div>
-  )
-}
 export default App
